5b6296e4f3216663ec3adf4c4837cf00bc09c314
KostiantynKrasnostup <vladovskiy462@gmail.com>
2024-09-16 17:46:03 +0300

    Add files via upload

---
+++ Приклад 7/Program.cs
@@ -0,0 +1,15 @@
+﻿Random o = new Random();
+List<int> chisla = new List<int>();
+for (int i = 0; i < 10; i++)
+{
+    chisla.Add(o.Next(1, 10));
+    Console.Write("{0}", chisla[i]);
+}
+Console.Write("\nВведіть індекс: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введіть число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+chisla.Insert(k, x);
+for (int i = 0; i < 10; i++)
+    Console.Write("{0}", chisla[i]);
+
