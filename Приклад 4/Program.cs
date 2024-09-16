6d78a8ae1e024caa69a80ea531434ea580f11385
KostiantynKrasnostup <vladovskiy462@gmail.com>
2024-09-16 17:45:10 +0300

    Add files via upload

---
+++ Приклад 4/Program.cs
@@ -0,0 +1,15 @@
+﻿Random o = new Random();
+List<int> chisla = new List<int>();
+for (int i = 0; i < 10; i++)
+{
+    chisla.Add(o.Next(1, 10));
+    Console.Write("{0}", chisla[i]);
+}
+Console.WriteLine();
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (int i = chisla.Count - 1; i >= 0; i--)
+    if (chisla[i] == x) chisla.Remove(x);
+
+for (int i = 0; i < chisla.Count; i++)
+    Console.Write("{0}", chisla[i]);
