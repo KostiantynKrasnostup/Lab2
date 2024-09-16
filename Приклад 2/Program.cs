15c1b841a10ebedd614e13642be3f942f3fb6093
KostiantynKrasnostup <vladovskiy462@gmail.com>
2024-09-16 17:43:33 +0300

    Add files via upload

---
+++ Приклад 2/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> chisla = new List<int> { 3, 14, 17, 6, 12, 18, 2, 2, 17, 10, 18, 1 };
+int mx = chisla[0];
+int nmx = 0;
+for (int i = 1; i < chisla.Count; i++)
+{
+    if (chisla[i] > mx)
+    {
+        mx = chisla[i]; 
+        nmx = i;       
+    }
+}
+Console.WriteLine("\n{0}, {1}", mx, nmx);
